76ccaaaa9a2764b0c0d5ea6a58faa6fb83538af7
Hen Shoster <hset2000@gmail.com>
2018-07-25 13:51:58 +0300

    Introduction to the project

--- mvc/view/templates/main_page/default_main_page_tpl.php
+++ mvc/view/templates/main_page/default_main_page_tpl.php
@@ -1,10 +1,11 @@
 <div class="container">
     <h1 class="display-4 m-3 text-center">Welcome to The School</h1>
+    <div class="dropdown-divider mb-4"></div>
     <div class="row">
-        <div class="col-6">
+        <div class="col-md-6">
             <?php include 'mvc/view/templates/main_page/general_info_tpl.html';?>
         </div>
-        <div class="col-6">
+        <div class="col-md-6">
             <p class="lead">File structure view:</h4>
             <ul>
                 <li><span class="text-success">The School</span>
